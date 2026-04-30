af5d57db4b619bba9baebc69b2b2301340b02dfd
Alexander James Phillips <ajp.global@gmail.com>
2026-04-30 11:56:21 +0100

    Add a link to subscribe for more info

--- src/sim_3d/simple_sim/FooterLinks.tsx
+++ src/sim_3d/simple_sim/FooterLinks.tsx
@@ -8,13 +8,14 @@ export function FooterLinks()
 {
     return <div id="footer_links">
         If you enjoy this please share it.
-        If you want to get involved you can <Link url="mailto:hello@wikisim.org">email us <MailLogo height={14} /></Link>,{" "}
+        You can <Link url="https://docs.google.com/forms/d/e/1FAIpQLSdKpO2KkvlXnhEoo9VejTID8tfGbHA_BEbZuFrsAku_TahH8w/viewform">Subscribe <MailLogo height={14} /></Link>,{" "}
+        <Link url="mailto:hello@wikisim.org">Email us <MailLogo height={14} /></Link>,{" "}
         <a href="" onClick={e =>
         {
             e.preventDefault()
             pub_sub.pub("show_info_and_data_sources", true)
-        }}>check the data <MagnifyingGlassLogo height={14} /></a>,{" "}
-        <Link url="https://github.com/AJamesPhillips/energy-explorer-v2/issues">add code <GitHubLogo height={14} /></Link>,{" "}
-        or <Link url="https://www.patreon.com/WikiSim">donate to support us to do more ❤️</Link>
+        }}>Check the data <MagnifyingGlassLogo height={14} /></a>,{" "}
+        <Link url="https://github.com/AJamesPhillips/energy-explorer-v2/issues">Code <GitHubLogo height={14} /></Link>,{" "}
+        or <Link url="https://www.patreon.com/WikiSim">Donate to support us to do more ❤️</Link>
     </div>
 }
